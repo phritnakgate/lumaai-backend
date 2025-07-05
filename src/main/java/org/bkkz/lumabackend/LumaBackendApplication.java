1dc2266ca339a267239bd2041468b63486a9aecc
phritnakgate <boss5500216@gmail.com>
2025-07-05 11:54:52 +0700

    initial commit

---
+++ src/main/java/org/bkkz/lumabackend/LumaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.bkkz.lumabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumaBackendApplication.class, args);
+    }
+
+}
